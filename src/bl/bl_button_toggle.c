d748ec84896eab647e4ac46c397317f7ec74c266
Vladyslav Daliavskyi <vladyslav.daliavskyi@su>
2020-06-02 10:07:37 +0300

    Code refactiring. Remove all interupts and delay function.

---
+++ src/bl/bl_button_toggle.c
@@ -0,0 +1,42 @@
+/*
+ * bl_button_toggle.c
+ *
+ *  Created on: 1 черв. 2020 р.
+ *      Author: vladyslav.daliavskyi
+ */
+
+#include "bl_button_toggle.h"
+
+
+
+void blButtonLedToggleInit(blButtonData* button,ledData* led){
+    drvInitStructButton initStr;
+    initStr.drvButtonPins = button->pin;
+    initStr.drvButtonPort = button->port;
+    initStr.drvButtonPullType = drvButtonHardPull;
+    initStr.drvButtonPuPd = drvButtonPuPdNo;
+    dvrButtonInit(&initStr);
+    drvLedInit(led->port,led->pin);
+}
+
+
+uint8_t count;
+uint8_t blIsButtonClick(blButtonData* button){
+    uint8_t code = 0;;
+    switch(drvIsButtonClicked(button->port,button->pin)){
+        case 1:
+            button->numberPress++;
+            break;
+        case 0:
+            count = button->numberPress;
+            if(count > 50) code = 1;
+            button->numberPress = 0;
+        }
+    return code;
+}
+
+void blToggleIfClick(blButtonData* button,ledData* led){
+    if(blIsButtonClick(button)==1){
+        drvLedToggle(led->port,led->pin);
+    }
+}
