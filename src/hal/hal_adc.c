5dee54f7d2a8b364216a6c067cac90f3a8645246
Vladyslav Daliavskyi <vladyslav.daliavskyi@su>
2020-07-20 18:14:50 +0300

    Added hal level for ADC

---
+++ src/hal/hal_adc.c
@@ -0,0 +1,168 @@
+/*
+ * hal_adc.c
+ *
+ *  Created on: 15 лип. 2020 р.
+ *      Author: vladyslav.daliavskyi
+ */
+
+#include "hal_adc.h"
+
+static const halADC_timerDef timer_def[halADC_timersCount] =
+{
+        {   TIM2, RCC_APB1ENR_TIM2EN, ADC_EXTERNALTRIGCONV_T2_TRGO    },
+        {   TIM4, RCC_APB1ENR_TIM4EN, ADC_EXTERNALTRIGCONV_T4_TRGO    },
+        {   TIM5, RCC_APB1ENR_TIM5EN, ADC_EXTERNALTRIGCONV_T5_TRGO    },
+        {   TIM6, RCC_APB1ENR_TIM6EN, ADC_EXTERNALTRIGCONV_T6_TRGO    }
+};
+
+static const halADC_def adc_def[halADC_Count] =
+{
+        {
+                ADC1,   RCC_APB2ENR_ADC1EN,
+                {
+                        {GPIOA, RCC_AHB1ENR_GPIOAEN, GPIO_PIN_0},
+                        {GPIOA, RCC_AHB1ENR_GPIOAEN, GPIO_PIN_1},
+                        {GPIOA, RCC_AHB1ENR_GPIOAEN, GPIO_PIN_2},
+                        {GPIOA, RCC_AHB1ENR_GPIOAEN, GPIO_PIN_3},
+                        {GPIOA, RCC_AHB1ENR_GPIOAEN, GPIO_PIN_4},
+                        {GPIOA, RCC_AHB1ENR_GPIOAEN, GPIO_PIN_5},
+                        {GPIOA, RCC_AHB1ENR_GPIOAEN, GPIO_PIN_6},
+                        {GPIOA, RCC_AHB1ENR_GPIOAEN, GPIO_PIN_7},
+                        {GPIOB, RCC_AHB1ENR_GPIOBEN, GPIO_PIN_0},
+                        {GPIOB, RCC_AHB1ENR_GPIOBEN, GPIO_PIN_1},
+                        {GPIOC, RCC_AHB1ENR_GPIOCEN, GPIO_PIN_0},
+                        {GPIOC, RCC_AHB1ENR_GPIOCEN, GPIO_PIN_1},
+                        {GPIOC, RCC_AHB1ENR_GPIOCEN, GPIO_PIN_2},
+                        {GPIOC, RCC_AHB1ENR_GPIOCEN, GPIO_PIN_3},
+                        {GPIOC, RCC_AHB1ENR_GPIOCEN, GPIO_PIN_4},
+                        {GPIOC, RCC_AHB1ENR_GPIOCEN, GPIO_PIN_5}
+                }
+        },
+        {
+                ADC2,   RCC_APB2ENR_ADC1EN,
+                {
+                        {GPIOA, RCC_AHB1ENR_GPIOAEN, GPIO_PIN_0},
+                        {GPIOA, RCC_AHB1ENR_GPIOAEN, GPIO_PIN_1},
+                        {GPIOA, RCC_AHB1ENR_GPIOAEN, GPIO_PIN_2},
+                        {GPIOA, RCC_AHB1ENR_GPIOAEN, GPIO_PIN_3},
+                        {GPIOA, RCC_AHB1ENR_GPIOAEN, GPIO_PIN_4},
+                        {GPIOA, RCC_AHB1ENR_GPIOAEN, GPIO_PIN_5},
+                        {GPIOA, RCC_AHB1ENR_GPIOAEN, GPIO_PIN_6},
+                        {GPIOA, RCC_AHB1ENR_GPIOAEN, GPIO_PIN_7},
+                        {GPIOB, RCC_AHB1ENR_GPIOBEN, GPIO_PIN_0},
+                        {GPIOB, RCC_AHB1ENR_GPIOBEN, GPIO_PIN_1},
+                        {GPIOC, RCC_AHB1ENR_GPIOCEN, GPIO_PIN_0},
+                        {GPIOC, RCC_AHB1ENR_GPIOCEN, GPIO_PIN_1},
+                        {GPIOC, RCC_AHB1ENR_GPIOCEN, GPIO_PIN_2},
+                        {GPIOC, RCC_AHB1ENR_GPIOCEN, GPIO_PIN_3},
+                        {GPIOC, RCC_AHB1ENR_GPIOCEN, GPIO_PIN_4},
+                        {GPIOC, RCC_AHB1ENR_GPIOCEN, GPIO_PIN_5}
+                }
+        },
+        {
+                ADC3,   RCC_APB2ENR_ADC1EN,
+                {
+                        {GPIOA, RCC_AHB1ENR_GPIOAEN, GPIO_PIN_0},
+                        {GPIOA, RCC_AHB1ENR_GPIOAEN, GPIO_PIN_1},
+                        {GPIOA, RCC_AHB1ENR_GPIOAEN, GPIO_PIN_2},
+                        {GPIOA, RCC_AHB1ENR_GPIOAEN, GPIO_PIN_3},
+                        {GPIOF, RCC_AHB1ENR_GPIOFEN, GPIO_PIN_6},
+                        {GPIOF, RCC_AHB1ENR_GPIOFEN, GPIO_PIN_7},
+                        {GPIOF, RCC_AHB1ENR_GPIOFEN, GPIO_PIN_8},
+                        {GPIOF, RCC_AHB1ENR_GPIOFEN, GPIO_PIN_9},
+                        {GPIOF, RCC_AHB1ENR_GPIOFEN, GPIO_PIN_10},
+                        {GPIOF, RCC_AHB1ENR_GPIOFEN, GPIO_PIN_3},
+                        {GPIOC, RCC_AHB1ENR_GPIOCEN, GPIO_PIN_0},
+                        {GPIOC, RCC_AHB1ENR_GPIOCEN, GPIO_PIN_1},
+                        {GPIOC, RCC_AHB1ENR_GPIOCEN, GPIO_PIN_2},
+                        {GPIOC, RCC_AHB1ENR_GPIOCEN, GPIO_PIN_3},
+                        {GPIOF, RCC_AHB1ENR_GPIOFEN, GPIO_PIN_4},
+                        {GPIOF, RCC_AHB1ENR_GPIOFEN, GPIO_PIN_5}
+                }
+        }
+};
+
+static const uint32_t channelValues[halADC_ChannelCount] = {
+        ADC_CHANNEL_0,  ADC_CHANNEL_1,  ADC_CHANNEL_2,  ADC_CHANNEL_3,
+        ADC_CHANNEL_4,  ADC_CHANNEL_5,  ADC_CHANNEL_6,  ADC_CHANNEL_7,
+        ADC_CHANNEL_8,  ADC_CHANNEL_9,  ADC_CHANNEL_10, ADC_CHANNEL_11,
+        ADC_CHANNEL_12, ADC_CHANNEL_13, ADC_CHANNEL_14, ADC_CHANNEL_15
+};
+
+void halADCInit(halADC_struct* adcStruct, halADC_initStruct* adcInitStruct) {
+    // enable RCC
+    uint8_t adcId = adcInitStruct->adc;
+    SET_BIT(RCC->APB2ENR, adc_def[adcId].adc_rcc);
+    SET_BIT(RCC->AHB1ENR, adc_def[adcId].adcPins[adcInitStruct->channel].port_rcc);
+
+    GPIO_InitTypeDef    pinInitStruct;
+    pinInitStruct.Mode      = GPIO_MODE_ANALOG;
+    pinInitStruct.Pin       = adc_def[adcId].adcPins[adcInitStruct->channel].pin;
+    pinInitStruct.Pull      = GPIO_NOPULL;
+    pinInitStruct.Speed     = GPIO_SPEED_FREQ_LOW;
+
+    HAL_GPIO_Init(adc_def[adcId].adcPins[adcInitStruct->channel].port, &pinInitStruct);
+
+    ADC_HandleTypeDef* adc = (ADC_HandleTypeDef*)adcStruct;
+    adc->Instance                   = adc_def[adcId].adc;
+    adc->Init.ClockPrescaler        = adcInitStruct->clockPrescaler;
+
+    adc->Init.Resolution            = adcInitStruct->dataSize;
+    adc->Init.NbrOfConversion       = CONVERTATIONS_NUMBER;
+    adc->Init.DataAlign             = adcInitStruct->dataAlign;
+    adc->Init.ScanConvMode          = DISABLE;
+    adc->Init.DiscontinuousConvMode = DISABLE;
+    adc->Init.DMAContinuousRequests = DISABLE;
+    adc->Init.EOCSelection          = ADC_EOC_SINGLE_CONV;
+
+    adc->Init.ContinuousConvMode    =
+                adcInitStruct->convMode == halADC_continuousConvMode ? ENABLE : DISABLE;
+
+    if (adcInitStruct->convMode == halADC_triggerConvMode) {
+
+        adc->Init.ExternalTrigConv =
+                timer_def[adcInitStruct->triggerTimerInitStruct.timer].trigger_value;
+        adc->Init.ExternalTrigConvEdge  = ADC_EXTERNALTRIGCONVEDGE_RISING;
+
+        SET_BIT(RCC->APB1ENR, timer_def[adcInitStruct->triggerTimerInitStruct.timer].timer_rcc);
+
+        adcStruct->timer.Instance   = timer_def[adcInitStruct->triggerTimerInitStruct.timer].timer;
+        TIM_Base_InitTypeDef* timer = &(adcStruct->timer.Init);
+
+        timer->Prescaler            = TIMER_PRESCALER;
+        timer->CounterMode          = TIM_COUNTERMODE_UP;
+        timer->Period               = EVENTS_ON_PERIOD / adcInitStruct->triggerTimerInitStruct.frequency;
+        timer->ClockDivision        = TIM_CLOCKDIVISION_DIV4;
+
+        timer->AutoReloadPreload    = TIM_AUTORELOAD_PRELOAD_DISABLE;
+
+        HAL_TIM_Base_Init(&(adcStruct->timer));
+
+    } else {
+
+        adc->Init.ExternalTrigConvEdge  = ADC_EXTERNALTRIGCONVEDGE_NONE;
+
+    }
+    HAL_ADC_Init(adc);
+
+    adcStruct->mode = adcInitStruct->convMode;
+
+    ADC_ChannelConfTypeDef channelConfig;
+    channelConfig.Channel = channelValues[adcInitStruct->channel];
+    channelConfig.Rank = CHANNEL_RANK;
+    channelConfig.SamplingTime = ADC_SAMPLETIME_3CYCLES;
+
+    HAL_ADC_ConfigChannel(adc, &channelConfig);
+}
+
+void halADCStart(halADC_struct* adcStruct) {
+    HAL_ADC_Start((ADC_HandleTypeDef*)adcStruct);
+}
+
+void halADCStop(halADC_struct* adcStruct) {
+    HAL_ADC_Stop((ADC_HandleTypeDef*)adcStruct);
+}
+
+uint16_t halADCGetValue(halADC_struct* adcStruct) {
+    return HAL_ADC_GetValue((ADC_HandleTypeDef*)adcStruct);
+}
---
+++ src/hal/hal_adc.h
@@ -0,0 +1,129 @@
+/*
+ * hal_adc.h
+ *
+ *  Created on: 15 лип. 2020 р.
+ *      Author: vladyslav.daliavskyi
+ */
+
+#ifndef HAL_HAL_ADC_H_
+#define HAL_HAL_ADC_H_
+
+#include "stm32f7xx.h"
+
+#define UINT16_MAX_VALUE        0xFF
+#define CHANNEL_RANK            1
+#define CONVERTATIONS_NUMBER    1
+
+#define EVENTS_ON_PERIOD        1000
+#define TIMER_PRESCALER         13499
+
+typedef enum {
+    halADC1,
+    halADC2,
+    halADC3,
+    halADC_Count
+} halADC;
+
+typedef enum {
+    halADC_Channel0,
+    halADC_Channel1,
+    halADC_Channel2,
+    halADC_Channel3,
+    halADC_Channel4,
+    halADC_Channel5,
+    halADC_Channel6,
+    halADC_Channel7,
+    halADC_Channel8,
+    halADC_Channel9,
+    halADC_Channel10,
+    halADC_Channel11,
+    halADC_Channel12,
+    halADC_Channel13,
+    halADC_Channel14,
+    halADC_Channel15,
+    halADC_ChannelCount
+} halADC_channel;
+
+typedef enum {
+    halADC_clockDiv2        = ADC_CLOCK_SYNC_PCLK_DIV2,
+    halADC_clockDiv4        = ADC_CLOCK_SYNC_PCLK_DIV4,
+    halADC_clockDiv6        = ADC_CLOCK_SYNC_PCLK_DIV6,
+    halADC_clockDiv8        = ADC_CLOCK_SYNC_PCLK_DIV8
+} halADC_clockPrescaler;
+
+typedef enum {
+    halADC_dataSize6b       = ADC_RESOLUTION_6B,
+    halADC_dataSize8b       = ADC_RESOLUTION_8B,
+    halADC_dataSize10b      = ADC_RESOLUTION_10B,
+    halADC_dataSize12b      = ADC_RESOLUTION_12B
+} halADC_dataSize;
+
+typedef enum {
+    halADC_dataAlignleft    = ADC_DATAALIGN_LEFT,
+    halADC_dataAlignRight   = ADC_DATAALIGN_RIGHT
+} halADC_dataAlign;
+
+typedef enum {
+    halADC_continuousConvMode = 0,
+    halADC_singleConvMode,
+    halADC_triggerConvMode
+} halADC_convertationMode;
+
+typedef enum {
+    halADC_timer2,
+    halADC_timer4,
+    halADC_timer5,
+    halADC_timer6,
+    halADC_timersCount
+} halADC_triggerTimer;
+
+typedef struct {
+    halADC_triggerTimer     timer;
+
+    uint32_t                frequency;
+} halADC_timerInitStruct;
+
+typedef struct {
+    halADC                  adc;
+    halADC_channel          channel;
+
+    halADC_clockPrescaler   clockPrescaler;
+    halADC_convertationMode convMode;
+    halADC_dataSize         dataSize;
+    halADC_dataAlign        dataAlign;
+
+    halADC_timerInitStruct  triggerTimerInitStruct;
+} halADC_initStruct;
+
+typedef struct {
+    ADC_HandleTypeDef       adcDef;
+    TIM_HandleTypeDef       timer;
+
+    halADC                  adc;
+    halADC_channel          channel;
+
+    halADC_convertationMode mode;
+
+} halADC_struct;
+
+typedef struct {
+    GPIO_TypeDef *          port;
+    uint32_t                port_rcc;
+    uint16_t                pin;
+} halADC_pin_def;
+
+typedef struct {
+    ADC_TypeDef *           adc;
+    uint32_t                adc_rcc;
+    halADC_pin_def          adcPins[halADC_ChannelCount];
+} halADC_def;
+
+typedef struct {
+    TIM_TypeDef *           timer;
+    uint32_t                timer_rcc;
+    uint32_t                trigger_value;
+} halADC_timerDef;
+
+void halADCInit(halADC_struct* adcStruct, halADC_initStruct* adcInitStruct);
+
+#endif /* HAL_HAL_ADC_H_ */
