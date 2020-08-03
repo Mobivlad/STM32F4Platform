8bda2aa6b25c9bd6e931fb8b5fc8cf8e5e7fb4b5
Vladyslav Daliavskyi <vladyslav.daliavskyi@su>
2020-08-03 11:50:09 +0300

    Added functions to FatFS util

--- src/utils/ul_fat_fs.c
+++ src/utils/ul_fat_fs.c
@@ -9,60 +9,13 @@
 
 static ulFatFS_struct* current_fatfs;
 
+/*
+ * Adapter block
+ */
+
 static volatile DSTATUS Stat = STA_NOINIT;
 
-static DSTATUS SD_initialize(BYTE lun);
-static DSTATUS SD_status(BYTE lun);
-static DRESULT SD_read(BYTE lun, BYTE *buff, DWORD sector, UINT count);
-static DRESULT SD_write(BYTE lun, const BYTE *buff, DWORD sector, UINT count);
-static DRESULT SD_ioctl(BYTE lun, BYTE cmd, void *buff);
-
 char SDPath[4];
-
-static const Diskio_drvTypeDef  SD_Driver =
-{
-  SD_initialize, SD_status, SD_read,
-#if  _USE_WRITE == 1
-  SD_write,
-#endif /* _USE_WRITE == 1 */
-
-#if  _USE_IOCTL == 1
-  SD_ioctl,
-#endif /* _USE_IOCTL == 1 */
-};
-
-void test() {
-    FATFS FatFs;
-    FIL fil; /* File object */
-    FRESULT fr; /* FatFs return code */
-
-    /* Gives a work area to the default drive */
-    FRESULT res = f_mount(&FatFs, "", 0);
-    if (res != FR_OK) {
-        Error_Handler();
-    }
-    f_mkdir("hello");
-    /* Open a text file */
-    res = f_open(&fil, "message.txt", FA_CREATE_ALWAYS);
-    if (res != FR_OK) {
-        Error_Handler();
-    }
-    /* Close the file */
-    res = f_close(&fil);
-    if (res != FR_OK) {
-        Error_Handler();
-    }
-    res = f_mount(0, "", 0);
-    if (res != FR_OK) {
-        Error_Handler();
-    }
-}
-
-void ulFatFSInit(ulFatFS_struct* fatfs) {
-    current_fatfs = fatfs;
-    fatfs->sd.sddriver = drvSDDriver1;
-    FATFS_LinkDriver(&SD_Driver, SDPath);
-}
 
 static DSTATUS SD_CheckStatus(BYTE lun) {
     Stat = STA_NOINIT;
@@ -160,3 +113,151 @@ static DRESULT SD_ioctl(BYTE lun, BYTE cmd, void *buff) {
     return res;
 }
 #endif /* _USE_IOCTL == 1 */
+
+static const Diskio_drvTypeDef  SD_Driver =
+{
+  SD_initialize, SD_status, SD_read,
+#if  _USE_WRITE == 1
+  SD_write,
+#endif /* _USE_WRITE == 1 */
+
+#if  _USE_IOCTL == 1
+  SD_ioctl,
+#endif /* _USE_IOCTL == 1 */
+};
+
+
+/*
+ * End adapter block
+ */
+
+ulFatFS_error ulFatFSInit(ulFatFS_struct* fatfs) {
+    current_fatfs = fatfs;
+    fatfs->sd.sddriver = drvSDDriver1;
+    return FATFS_LinkDriver(&SD_Driver, SDPath)==0 ? ulFatFS_OK : ulFatFS_DISK_ERR;
+}
+
+// test function for FatFS
+void test() {
+    FIL fil; /* File object */
+    DIR dir;
+    FRESULT fr; /* FatFs return code */
+    FILINFO fno;
+
+    /* Gives a work area to the default drive */
+    FRESULT res = f_mount(&current_fatfs->FatFs, "0:", 0);
+    if (res != FR_OK) {
+        Error_Handler();
+    }
+    uint8_t count = 0;
+    res = f_findfirst(&dir, &fno, "folder/f", "text*.txt" );
+    if (res != FR_OK) {
+        Error_Handler();
+    } else {
+
+        while(res == FR_OK && fno.fname[0]){
+            count++;
+            res = f_findnext(&dir, &fno);
+        }
+    }
+    f_mkdir("somedir");
+    /* Open a text file */
+    res = f_open(&fil, "hello/message.txt", FA_CREATE_ALWAYS);
+    if (res != FR_OK) {
+        Error_Handler();
+    }
+
+
+    /* Close the file */
+    res = f_close(&fil);
+    if (res != FR_OK) {
+        Error_Handler();
+    }
+    res = f_mount(0, "0:", 0);
+    if (res != FR_OK) {
+        Error_Handler();
+    }
+}
+
+ulFatFS_error ulFatFSMountSD(ulFatFS_struct* fatfs) {
+    return fatfs == NULL ? ulFatFS_DATA_NULL_POINT : f_mount(&current_fatfs->FatFs, "", 0);
+}
+
+ulFatFS_error ulFatFSUnmountSD(ulFatFS_struct* fatfs) {
+
+    return fatfs == NULL ? ulFatFS_DATA_NULL_POINT : f_mount(NULL, "", 0);
+}
+
+ulFatFS_error ulFatFSCreateFolder(ulFatFS_struct* fatfs, char* folderName) {
+    return fatfs == NULL ? ulFatFS_DATA_NULL_POINT : f_mkdir(folderName);
+}
+
+ulFatFS_error ulFatFSFindCount(ulFatFS_struct* fatfs, char* folderPath, char* fileMask,
+        uint8_t* count) {
+    if (fatfs == NULL || folderPath == NULL || fileMask == NULL || count == NULL) {
+        return ulFatFS_DATA_NULL_POINT;
+    }
+    DIR dir;
+    FILINFO fno;
+    uint8_t tmp_count = 0;
+    FRESULT fr = f_findfirst(&dir, &fno, folderPath, fileMask);
+    if (fr != (FRESULT) ulFatFS_OK) {
+        return fr;
+    }
+    while (*(fno.fname)) {
+        if (fr != (FRESULT) ulFatFS_OK) {
+            return fr;
+        }
+        tmp_count++;
+        fr = f_findnext(&dir, &fno);
+    }
+    fr = f_closedir(&dir);
+    *count = (fr == (FRESULT) ulFatFS_OK) ? tmp_count : SD_ERROR_VALUE;
+    return fr;
+}
+
+ulFatFS_error ulFatFSOpenFile(ulFatFS_struct* fatfs, ulFatFS_File_t* file, char* fileName,
+        ulFatFS_filePosix posix) {
+    return f_open(file, fileName, posix);
+}
+
+ulFatFS_error ulFatFSWrite(ulFatFS_struct* fatfs, ulFatFS_File_t* file, uint8_t* data, uint16_t dataLen) {
+    if (fatfs == NULL || data == NULL || file == NULL) {
+        return ulFatFS_DATA_NULL_POINT;
+    }
+    uint16_t writtenBytes = 0;
+    FRESULT fr = f_write(file, data, dataLen, (unsigned int*) &writtenBytes);
+    if ( fr!= (FRESULT) ulFatFS_OK ) {
+        return fr;
+    }
+    return writtenBytes == dataLen ? ulFatFS_OK : ulFatFS_COMPLETE_OPERATION_ERROR;
+}
+
+ulFatFS_error ulFatFSWriteString(ulFatFS_struct* fatfs, ulFatFS_File_t* file, const char* str) {
+    if (fatfs == NULL || str == NULL || file == NULL) {
+        return ulFatFS_DATA_NULL_POINT;
+    }
+    uint16_t writtenBytes = 0;
+    uint16_t dataLen = strlen(str);
+    FRESULT fr = f_write(file, str, dataLen, (unsigned int*) &writtenBytes);
+    if ( fr!= (FRESULT) ulFatFS_OK ) {
+        return fr;
+    }
+    return writtenBytes == dataLen ? ulFatFS_OK : ulFatFS_COMPLETE_OPERATION_ERROR;
+}
+
+ulFatFS_error ulFatFSRead(ulFatFS_struct* fatfs, ulFatFS_File_t* file, uint8_t* data, uint16_t dataLen) {
+    if (fatfs == NULL || data == NULL || file == NULL) {
+        return ulFatFS_DATA_NULL_POINT;
+    }
+    uint16_t readBytes = 0;
+    FRESULT fr = f_read(file, data, dataLen, (unsigned int*) &readBytes);
+    if ( fr!= (FRESULT) ulFatFS_OK ) {
+        return fr;
+    }
+    return readBytes == dataLen ? ulFatFS_OK : ulFatFS_COMPLETE_OPERATION_ERROR;
+}
+
+ulFatFS_error ulFatFSCloseFile(ulFatFS_struct* fatfs, ulFatFS_File_t* file) {
+    return f_close(file);
+}
--- src/utils/ul_fat_fs.h
+++ src/utils/ul_fat_fs.h
@@ -8,24 +8,66 @@
 #ifndef UTILS_UL_FAT_FS_H_
 #define UTILS_UL_FAT_FS_H_
 
-#include "drv_sd_driver.h"
+#include <drv_sd_sdio.h>
+#include <string.h>
 #include "ff_gen_drv.h"
 #include "ffconf.h"
 
 #if defined(SDMMC_DATATIMEOUT)
-#define SD_TIMEOUT SDMMC_DATATIMEOUT
+    #define SD_TIMEOUT SDMMC_DATATIMEOUT
 #elif defined(SD_DATATIMEOUT)
-#define SD_TIMEOUT SD_DATATIMEOUT
+    #define SD_TIMEOUT SD_DATATIMEOUT
 #else
-#define SD_TIMEOUT 30 * 1000
+    #define SD_TIMEOUT 30 * 1000
 #endif
-#define SD_BLOCK_SIZE 512
+
+#define SD_BLOCK_SIZE            512
+#define SD_ERROR_VALUE           -1
+
+#define ulFatFS_File_t           FIL
+#define ulFatFS_Directory_t      DIR
 
 typedef struct {
     drvSDDriver_struct sd;
+    FATFS FatFs;
 } ulFatFS_struct;
 
-void ulFatFSInit(ulFatFS_struct* fatfs);
+typedef enum {
+    ulFatFS_OK                   = FR_OK,
+    ulFatFS_DISK_ERR             = FR_DISK_ERR,
+    ulFatFS_INT_ERR              = FR_INT_ERR,
+    ulFatFS_NOT_READY            = FR_NOT_READY,
+    ulFatFS_NO_FILE              = FR_NO_FILE,
+    ulFatFS_NO_PATH              = FR_NO_PATH,
+    ulFatFS_INVALID_NAME         = FR_INVALID_NAME,
+    ulFatFS_DENIED               = FR_DENIED,
+    ulFatFS_EXIST                = FR_EXIST,
+    ulFatFS_INVALID_OBJECT       = FR_INVALID_OBJECT,
+    ulFatFS_WRITE_PROTECTED      = FR_WRITE_PROTECTED,
+    ulFatFS_INVALID_DRIVE        = FR_INVALID_DRIVE,
+    ulFatFS_NOT_ENABLED          = FR_NOT_ENABLED,
+    ulFatFS_NO_FILESYSTEM        = FR_NO_FILESYSTEM,
+    ulFatFS_TIMEOUT              = FR_TIMEOUT,
+    ulFatFS_LOCKED               = FR_LOCKED,
+    ulFatFS_NOT_ENOUGH_CORE      = FR_NOT_ENOUGH_CORE,
+    ulFatFS_TOO_MANY_OPEN_FILES  = FR_TOO_MANY_OPEN_FILES,
+    ulFatFS_INVALID_PARAMETER    = FR_INVALID_PARAMETER,
+    ulFatFS_DATA_NULL_POINT,
+    ulFatFS_COMPLETE_OPERATION_ERROR
+} ulFatFS_error;
+
+typedef enum {
+    R   = FA_READ,
+    RW  = R | FA_WRITE,
+    CW  = FA_CREATE_ALWAYS | FA_WRITE,
+    CWR = CW | R,
+    A   = FA_OPEN_APPEND | FA_READ,
+    AR  = A  | FA_READ,
+    OW  = FA_CREATE_NEW | FA_WRITE,
+    OWR = OW | R
+} ulFatFS_filePosix;
+
+ulFatFS_error ulFatFSInit(ulFatFS_struct* fatfs);
 
 void test();
 
